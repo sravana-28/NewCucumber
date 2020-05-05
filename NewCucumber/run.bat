a3be68cb04f62136803de2e72bbeebf8185beb43
sravana-28 <sravanachippala@gmail.com>
2020-05-05 18:17:18 -0500

    this is my new commit in newcucumber

---
+++ NewCucumber/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\daya\eclipsenew\NewCucumber
+mvn clean install  
---
+++ NewCucumber/src/test/java/stepDefinitions/BaseClass.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import java.util.Properties;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+
+import pageObjects.AddcustomerPage;
+import pageObjects.LoginPage;
+import pageObjects.SearchCustomerPage;
+
+public class BaseClass {
+	
+	public WebDriver driver;
+	public LoginPage lp;
+	public AddcustomerPage addCust;
+	public SearchCustomerPage searchCust;
+	public Logger logger;
+	public Properties configProp;
+	
+
+	//Created for generating random string for Unique email
+	public static String randomstring() {
+		String generatedstring1=RandomStringUtils.randomAlphabetic(5);
+		return(generatedstring1);
+		
+	}
+	
+}
